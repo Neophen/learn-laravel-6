56bd97d47b76301ef177047faeb3a5adf8a3893b
Mykolas Mankevicius <mykolasm@wix.com>
2019-11-02 07:14:20 +0200

    replace links with resource links

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/articles', function () {
-//     // $articles = \App\Article::latest()->get();
-//     // $articles = \App\Article::take(2)->get();
-//     // $articles = \App\Article::paginate(2);
-
-//     return view('articles.index', [
-//         'articles' => \App\Article::latest()->get()
-//     ]);
-// });
-
-Route::get('/articles', 'ArticlesController@index')->name('articles.index');
-Route::get('/articles/create', 'ArticlesController@create')->name('articles.create');
-Route::get('/articles/{article}/edit', 'ArticlesController@edit')->name('articles.edit');
-Route::get('/articles/{article}', 'ArticlesController@show')->name('articles.show');
-
-Route::post('/articles', 'ArticlesController@store')->name('articles.store');
-Route::put('/articles/{article}', 'ArticlesController@update')->name('articles.update');
-Route::delete('/articles/{article}', 'ArticlesController@destroy')->name('articles.destroy');
+Route::resource('articles', 'ArticlesController');
